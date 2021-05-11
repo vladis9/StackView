25483fa379344ded1a51a71ebbd0c9fcfa6c3630
Vlad Zinevich <vladisznv@gmail.com>
2021-05-11 14:22:25 +0300

    Initial Commit

---
+++ Stack View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stack View
+//
+//  Created by Vlad Zinevich on 11.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
